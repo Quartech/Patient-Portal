7be8ab6b4ed4d9e48aeaaa04edd46e760ad40524
bradhead <brad.head@quartech.com>
2019-04-16 10:41:46 -0700

    fixed connections layout

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -18,22 +18,19 @@
     <p>Enjoy all the benefits and value that third-party health apps provide by
       connecting them to your health data</p>
     <h4>You have two connected apps</h4>
+    <p>&nbsp;</p>
     <b-row>
-      <b-col>
-        <img src="https://images.ctfassets.net/1izjqx4qtt8c/1NogrRoCWomi6Ksecqm6KS/0a863bb2d152c76713626ec4fc1ee9d9/THB_logo_hor_pur_RGB.png"/>
-      </b-col>
       <b-col cols="5">
+        <img src="https://images.ctfassets.net/1izjqx4qtt8c/1NogrRoCWomi6Ksecqm6KS/0a863bb2d152c76713626ec4fc1ee9d9/THB_logo_hor_pur_RGB.png" height="30"/>
        <h6>Connect with Babylon to share all your health data</h6>
-       <h4>[Disconnect Babylon]</h4>
+       <h5>[Disconnect Babylon]</h5>
        </b-col>
     </b-row>
     <b-row>
-      <b-col>
-        <img src="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/ios/built_in/ios9-health-app-icon.png"/>
-      </b-col>
       <b-col cols="5">
+        <img src="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/ios/built_in/ios9-health-app-icon.png" height="30"/>
        <h6>Connect with Apple Health to share all your health data to view progress</h6>
-       <h4>[Disconnect Apple Health]</h4>
+       <h5>[Disconnect Apple Health]</h5>
        </b-col>
     </b-row>
     <h1>Delegation</h1>
