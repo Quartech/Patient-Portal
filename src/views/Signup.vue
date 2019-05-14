b9cf62d4e1027258934354cfaa3deb614aa164aa
King <Wade.King@quartech.com>
2019-05-14 15:07:56 -0700

    a makeshift signup page

---
+++ src/views/Signup.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class = "container">
+        <p>Hello world</p>
+        <form action="http://localhost:3001/data" method="post">
+            <input type="text" name="firstname"><br>
+            <input type="text" name="lastname"><br>
+            <input type="text" name="email"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</template>
